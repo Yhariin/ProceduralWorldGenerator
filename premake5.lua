db44c611865fe4f680f82e495e75338f8758f970
Yhariin <suarezcristian137@gmail.com>
2024-05-21 21:28:13 -0400

    Converted to Premake build system

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+workspace "ProceduralWorldGenerator"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.architecture}"
+
+project "ProceduralWorldGenerator"
+    location "ProceduralWorldGenerator"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+    defines
+    {
+        "_CONSOLE"
+    }
+
+    files
+    {
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/src/**.hpp"
+    }
+
+
+    filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+        flags { "MultiProcessorCompile" }
+
+    filter "configurations:Debug"
+        defines { "_DEBUG" }
+        runtime "Debug"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        runtime "Release"
