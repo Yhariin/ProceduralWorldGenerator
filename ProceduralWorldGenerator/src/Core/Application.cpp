d79f60446c816a0f0d3a3f4d0567290dba6e0b0c
Yhariin <suarezcristian137@gmail.com>
2024-06-03 21:06:19 -0400

    Added comment detailing how callback function binding works

--- ProceduralWorldGenerator/src/Core/Application.cpp
+++ ProceduralWorldGenerator/src/Core/Application.cpp
@@ -14,7 +14,12 @@ Application::Application()
 	m_Window = std::make_unique<Window>();
 
 	// Set Window Callback here
-	m_Window->SetEventCallback(std::bind(&Application::OnEvent, this, std::placeholders::_1));
+	// We want to set m_Window's event callback to Application's OnEvent function
+	// however setEventCallback expects a function with the signature void(*fn)(Event&).
+	// This is a problem because our OnEvent function has the signature void(*fn)(this, Event&).
+	// We can use a lambda capture with this to then return OnEvent. Alternatively you can use std::bind,
+	// m_Window->SetEventCallback(std::bind(&Application::OnEvent, this, std::placeholders::_1));
+	m_Window->SetEventCallback([this](Event& e) { return this->OnEvent(e); });
 
 	// Initialize Renderer here
 }
@@ -27,7 +32,7 @@ Application::~Application()
 
 void Application::OnEvent(Event& e)
 {
-	LOG_INFO(e.ToString());
+	//LOG_INFO(e.ToString());
 }
 
 void Application::Run()
